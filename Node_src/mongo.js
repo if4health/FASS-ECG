5cd1fdffb8736f6b863a5d1217298a07fe1ba172
Cássio Ribeiro <cassio@gomining.com.br>
2022-03-29 15:15:20 -0300

    deploy heroku

--- Node_src/mongo.js
+++ Node_src/mongo.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose")
 
 const { DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME } = process.env
 
-let connString = process.env.DB_URI;
-
+// let connString = process.env.DB_URI;
+let connString = 'mongodb+srv://tcc:tccSenha@cluster0.tx7an.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 class dbConnect{
     
     constructor(){
